2032ec96bdd3aa5213caf63ef2458cb0af12be40
Igor Kleshev <membship@ya.ru>
2016-10-02 21:00:13 +0600

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,15 +1,23 @@
 jQuery(document).ready(function($){
 
-$('.lang-inline li:first-child a').text('Eng');
-$('.lang-inline li:nth-child(2) a').text('Укр');
-$('.lang-inline li:nth-child(3) a').text('Рус');
+	$('.lang-inline li:first-child a').text('Eng');
+	$('.lang-inline li:nth-child(2) a').text('Укр');
+	$('.lang-inline li:nth-child(3) a').text('Рус');
 
 
-$('.mnwall-readmore a').text('Подробнее');
+	$('.mnwall-readmore a').text('Подробнее');
 
 
 
 
+	$('.uk-overlay.uk-overlay-hover .uk-position-cover').click(function (eventObject) {
+		eventObject.preventDefault();
+		alert($(this).attr('href'));
+	});
+
+
+
+	
 
 	
 
