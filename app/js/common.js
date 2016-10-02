5c4e92d2306d43f0bbafaf974ba720845ac046d7
Igor Kleshev <membship@ya.ru>
2016-10-02 16:00:01 +0600

    commit

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($){
 
-$('.lang-inline li:first-child a').text('EN');
-$('.lang-inline li:nth-child(2) a').text('AR');
-$('.lang-inline li:nth-child(3) a').text('RU');
+$('.lang-inline li:first-child a').text('Eng');
+$('.lang-inline li:nth-child(2) a').text('Укр');
+$('.lang-inline li:nth-child(3) a').text('Рус');
 
 
 
